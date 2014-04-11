ae88c9891c66765847c79bfbbcfa24215e0a3284
Mickaël Canévet <mickael.canevet@camptocamp.com>
2014-04-11 15:20:00 +0200

    Fix absents() on ruby 1.8.7

--- lib/puppet/parser/functions/absents.rb
+++ lib/puppet/parser/functions/absents.rb
@@ -3,8 +3,9 @@ module Puppet::Parser::Functions
       Returns a hash of the elements that have ensure == absent.
     EOS
   ) do |args|
-    raise(Puppet::ParseError, "absents(): Wrong number of arguments given") if args.size != 1
+    raise(Puppet::ArgumentError, "absents(): Wrong number of arguments given") if args.size != 1
+    raise(Puppet::ArgumentError, "absents(): First parameter must be a hash") if args[0].class != Hash
 
-    args[0].select{ |k, v| v['ensure'] == 'absent' }
+    Hash[args[0].select{ |k, v| v['ensure'] == 'absent' }]
   end
 end
