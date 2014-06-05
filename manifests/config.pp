143340d1bde87b4372e859cf681168b613d43aa7
Simon Séhier <simon.sehier@camptocamp.com>
2014-06-05 11:39:46 +0200

    fix augeas ressource duplicate name + add default case for unknow osfamily

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,3 +1,4 @@
+# See README for doc
 class accounts::config {
 
   case $::osfamily {
@@ -31,13 +32,21 @@ class accounts::config {
         }
       }
       if $::accounts::start_gid != undef {
-        augeas {'Set first and last uids':
+        augeas {'Set first and last gids':
           incl    => '/etc/login.defs',
           lens    => 'Login_Defs.lns',
           changes => [
             "set GID_MIN ${::accounts::start_gid}",
           ],
         }
+      }
+    }
+    default: {
+      if $::accounts::start_uid != undef {
+        fail("I don't know how to set first uids on osfamily ${::osfamily}")
+      }
+      if $::accounts::start_gid != undef {
+        fail("I don't know how to set first gids on osfamily ${::osfamily}")
       }
     }
 
