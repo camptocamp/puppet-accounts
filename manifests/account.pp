588dfc3ad31640a0d65b60c955ea68eb1f3d5f25
Mickaël Canévet <mickael.canevet@camptocamp.com>
2014-06-24 17:43:32 +0200

    #187967 : droits pour synchro intranet -> extranet

--- manifests/account.pp
+++ manifests/account.pp
@@ -49,8 +49,22 @@ define accounts::account(
         fail 'authorized_keys must be a String, an Array or a Hash'
       }
       if $::accounts::ssh_keys[$name] != undef and $::accounts::ssh_keys[$name]['private'] != undef {
-        file { "/home/${user}/.ssh/id_rsa":
-          content => $::accounts::ssh_keys[$name]['private'],
+        # NOTE: getparam(User[$user], 'home') would do the trick to fetch
+        # user's home dir, but it depends on parsing order
+        #
+        # $home = getparam(User[$user], 'home')
+        # file { "${home}/.ssh/id_rsa":
+        #   content => $::accounts::ssh_keys[$name]['private'],
+        # }
+        #
+        # Another solution would be to use puppetdbquery:
+        #
+        # $ret = query_resources("fqdn='${::fqdn}'", "User['${user}']")
+        # $home = $ret[$::fqdn][0]['parameters']['home']
+        #
+        # TODO: Fix unless so that it replaces the key
+        exec { "/bin/echo '${::accounts::ssh_keys[$name]['private']}' > ~${user}/.ssh/id_rsa":
+          unless => "test -f ~${user}/.ssh/id_rsa",
         }
       }
     }
