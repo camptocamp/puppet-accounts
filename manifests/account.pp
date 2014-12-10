7e4576c2d298094a22d9b367b5defd908ee6e440
Simon Séhier <simon.sehier@camptocamp.com>
2014-12-10 12:28:05 +0100

    Change exec resource name when creating RSA private key
      - avoid ~ in ressource name (puppetdb doesn't like it atm)
      - avoid having the private key in the resource name/tag

--- manifests/account.pp
+++ manifests/account.pp
@@ -91,8 +91,9 @@ define accounts::account(
         # $home = $ret[$::fqdn][0]['parameters']['home']
         #
         # TODO: Fix unless so that it replaces the key
-        exec { "/bin/echo '${::accounts::ssh_keys[$name]['private']}' > ~${user}/.ssh/id_rsa":
-          unless => "/usr/bin/test -f ~${user}/.ssh/id_rsa",
+        exec { "put ssh private key ${name} for user ${user}":
+          command => "/bin/echo '${::accounts::ssh_keys[$name]['private']}' > ~${user}/.ssh/id_rsa",
+          unless  => "/usr/bin/test -f ~${user}/.ssh/id_rsa",
         }
       }
     }
