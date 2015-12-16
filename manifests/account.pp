a22b4d9961050f3d9ff48fab084a64ffae597e6f
Mickaël Canévet <mickael.canevet@camptocamp.com>
2015-12-16 10:39:13 +0100

    Refactor

--- manifests/account.pp
+++ manifests/account.pp
@@ -1,23 +1,45 @@
 # See README.md for details.
 define accounts::account(
-  $ensure                   = present,
+  $ensure                   = 'present',
+  $allowdupe                = undef,
+  $attribute_membership     = undef,
+  $attributes               = undef,
+  $auth_membership          = undef,
+  $auths                    = undef,
   $comment                  = undef,
+  $expiry                   = undef,
+  $forcelocal               = undef,
+  $gid                      = $::accounts::groups_membership,
+  $groups                   = undef,
+  $home                     = undef,
+  $ia_load_module           = undef,
+  $iterations               = undef,
+  $key_membership           = undef,
+  $keys                     = undef,
+  $loginclass               = undef,
+  $managehome               = $::accounts::managehome,
+  $membership               = undef,
+  $password                 = undef,
+  $password_max_age         = undef,
+  $password_min_age         = undef,
+  $profile_membership       = undef,
+  $profiles                 = undef,
+  $project                  = undef,
+  $provider                 = undef,
+  $purge_ssh_keys           = $::accounts::purge_ssh_keys,
+  $role_membership          = undef,
+  $roles                    = undef,
+  $salt                     = undef,
+  $shell                    = $::accounts::shell,
+  $system                   = undef,
+  $uid                      = undef,
+
   $user                     = $name,
-  $groups                   = [],
-  $groups_membership        = $::accounts::groups_membership,
   $authorized_keys          = [],
   $authorized_keys_target   = undef,
-  $purge_ssh_keys           = $::accounts::purge_ssh_keys,
   $ssh_authorized_key_title = $::accounts::ssh_authorized_key_title,
-  $shell                    = $::accounts::shell,
-  $home                     = undef,
-  $managehome               = $::accounts::managehome,
-  $password                 = undef,
-  $uid                      = undef,
-  $gid                      = undef,
   $ssh_options              = undef,
 ) {
-  $account = $user # for strformat mapping...
   if $user =~ /^@(\S+)$/ {
     if ! has_key($::accounts::usergroups, $1) {
       fail "Can't find usergroup : ${1}"
@@ -27,62 +49,79 @@ define accounts::account(
       $::accounts::usergroups[$1],
       {
         ensure                   => $ensure,
+        allowdupe                => $allowdupe,
+        attribute_membership     => $attribute_membership,
+        attributes               => $attributes,
+        auth_membership          => $auth_membership,
+        auths                    => $auths,
         comment                  => $comment,
+        expiry                   => $expiry,
+        forcelocal               => $forcelocal,
+        gid                      => $gid,
         groups                   => $groups,
+        home                     => $home,
+        ia_load_module           => $ia_load_module,
+        iterations               => $iterations,
+        key_membership           => $key_membership,
+        keys                     => $keys,
+        loginclass               => $loginclass,
+        managehome               => $managehome,
+        membership               => $membership,
+        password                 => $password,
+        password_max_age         => $password_max_age,
+        password_min_age         => $password_min_age,
+        profile_membership       => $profile_membership,
+        profiles                 => $profiles,
+        project                  => $project,
+        provider                 => $provider,
+        purge_ssh_keys           => $purge_ssh_keys,
+        role_membership          => $role_membership,
+        roles                    => $roles,
+        salt                     => $salt,
+        shell                    => $shell,
+        system                   => $system,
+        uid                      => $uid,
         authorized_keys          => $authorized_keys,
         authorized_keys_target   => $authorized_keys_target,
-        purge_ssh_keys           => $purge_ssh_keys,
         ssh_authorized_key_title => $ssh_authorized_key_title,
-        shell                    => $shell,
-        home                     => $home,
-        password                 => $password,
-        uid                      => $uid,
-        gid                      => $gid,
+        ssh_options              => $ssh_options,
       }
     )
   } else {
-    if has_key($::accounts::users, $user) {
-      if $purge_ssh_keys and $authorized_keys_target {
-        $_purge_ssh_keys = strformat($authorized_keys_target)
-      } else {
-        $_purge_ssh_keys = $purge_ssh_keys
-      }
-      $_home = $home ? {
-        undef   => "/home/${$user}",
-        default => $home,
-      }
-
-      $hash = merge(
-        {
-          ensure     => $ensure,
-          comment    => $comment,
-          groups     => $groups,
-          home       => $_home,
-          password   => $password,
-          managehome => $managehome,
-          membership => $groups_membership,
-          shell      => $shell,
-          uid        => $uid,
-          gid        => $gid,
-        },
-        $::accounts::users[$name]
-      )
-
-      if versioncmp($::puppetversion, '3.6.0') >= 0 {
-        $_hash = merge(
-          $hash,
-          {
-            purge_ssh_keys => $_purge_ssh_keys,
-          }
-        )
-      } else {
-        $_hash = $hash
-      }
-      ensure_resource(
-        user,
-        $user,
-        $_hash
-      )
+    user { $name:
+      ensure               => pick($::accounts::users[$name]['ensure'], $ensure),
+      allowdupe            => pick($::accounts::users[$name]['allowdupe'], $allowdupe),
+      attribute_membership => pick($::accounts::users[$name]['attribute_membership'], $attribute_membership),
+      attributes           => pick($::accounts::users[$name]['attributes'], $attributes),
+      auth_membership      => pick($::accounts::users[$name]['auth_membership'], $auth_membership),
+      auths                => pick($::accounts::users[$name]['auths'], $auths),
+      comment              => pick($::accounts::users[$name]['comment'], $comment),
+      expiry               => pick($::accounts::users[$name]['expiry'], $expiry),
+      forcelocal           => pick($::accounts::users[$name]['forcelocal'], $forcelocal),
+      gid                  => pick($::accounts::users[$name]['gid'], $gid),
+      groups               => pick($::accounts::users[$name]['groups'], $groups),
+      home                 => pick($::accounts::users[$name]['home'], $home, "/home/${name}"),
+      ia_load_module       => pick($::accounts::users[$name]['ia_load_module'], $ia_load_module),
+      iterations           => pick($::accounts::users[$name]['iterations'], $iterations),
+      key_membership       => pick($::accounts::users[$name]['key_membership'], $key_membership),
+      keys                 => pick($::accounts::users[$name]['keys'], $keys),
+      loginclass           => pick($::accounts::users[$name]['loginclass'], $loginclass),
+      managehome           => pick($::accounts::users[$name]['managehome'], $managehome),
+      membership           => pick($::accounts::users[$name]['membership'], $membership),
+      password             => pick($::accounts::users[$name]['password'], $password),
+      password_max_age     => pick($::accounts::users[$name]['password_max_age'], $password_max_age),
+      password_min_age     => pick($::accounts::users[$name]['password_min_age'], $password_min_age),
+      profile_membership   => pick($::accounts::users[$name]['profile_membership'], $profile_membership),
+      profiles             => pick($::accounts::users[$name]['profiles'], $profiles),
+      project              => pick($::accounts::users[$name]['project'], $project),
+      provider             => pick($::accounts::users[$name]['provider'], $provider),
+      purge_ssh_keys       => pick($::accounts::users[$name]['purge_ssh_keys'], $purge_ssh_keys),
+      role_membership      => pick($::accounts::users[$name]['role_membership'], $role_membership),
+      roles                => pick($::accounts::users[$name]['roles'], $roles),
+      salt                 => pick($::accounts::users[$name]['salt'], $salt),
+      shell                => pick($::accounts::users[$name]['shell'], $shell),
+      system               => pick($::accounts::users[$name]['system'], $system),
+      uid                  => pick($::accounts::users[$name]['uid'], $uid),
     }
 
     if $ensure != absent {
